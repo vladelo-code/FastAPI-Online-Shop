1c9f6eff721449d47263e17a39eb51b57fc1ab32
vladelo <vlad.lakhtion@gmail.com>
2025-06-11 21:42:49 +0300

    init commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import uvicorn
+from fastapi import FastAPI
+from ecommerce.user.router import router as user_router
+
+app = FastAPI(title='Vladelo Online Shop', version='0.0.1')
+app.include_router(user_router)
+
+if __name__ == '__main__':
+    uvicorn.run('main:app', host='0.0.0.0', port=8000, reload=True)
