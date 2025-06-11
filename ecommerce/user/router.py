cb4f490e58360c924da17653519502dd85f370bc
vladelo <vlad.lakhtion@gmail.com>
2025-06-11 21:45:31 +0300

    adding user router

---
+++ ecommerce/user/router.py
@@ -0,0 +1,19 @@
+from fastapi import APIRouter, Depends, HTTPException, status, Response
+from sqlalchemy.orm import Session
+
+from ecommerce import db
+from . import shema, services, validator
+
+router = APIRouter(tags=["Users"], prefix="/user")
+
+
+@router.post('/', status_code=status.HTTP_201_CREATED)
+async def create_user_registration(request: shema.User, database: Session = Depends(db.get_db)):
+    user = await validator.verify_email_exist(email=request.email, db_session=database)
+
+    if user:
+        raise HTTPException(status_code=400, detail="This user with this email already exist in the system.")
+
+    new_user = await services.new_user_register(request=request, database=database)
+
+    return new_user
