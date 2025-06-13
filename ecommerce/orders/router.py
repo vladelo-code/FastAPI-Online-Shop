dee56fac8e65aac24bbd83bb440b2402360ff8c9
vladelo <vlad.lakhtion@gmail.com>
2025-06-13 12:29:45 +0300

    adding router to get order list

--- ecommerce/orders/router.py
+++ ecommerce/orders/router.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
+from typing import List
 
 from ecommerce import db
 from . import shema
@@ -11,4 +12,10 @@ router = APIRouter(tags=["Orders"], prefix="/orders")
 @router.post('/', status_code=status.HTTP_201_CREATED, response_model=shema.ShowOrder)
 async def initiate_order_processing(database: Session = Depends(db.get_db)):
     result = await services.initiate_order(database)
+    return result
+
+
+@router.get('/', status_code=status.HTTP_200_OK, response_model=List[shema.ShowOrder])
+async def get_order_list(database: Session = Depends(db.get_db)):
+    result = await services.get_order_listing(database)
     return result
--- main.py
+++ main.py
@@ -4,11 +4,13 @@ from fastapi import FastAPI
 from ecommerce.user.router import router as user_router
 from ecommerce.products.router import router as product_router
 from ecommerce.cart.router import router as cart_router
+from ecommerce.orders.router import router as order_router
 
 app = FastAPI(title='Vladelo Online Shop', version='0.0.1')
 app.include_router(user_router)
 app.include_router(product_router)
 app.include_router(cart_router)
+app.include_router(order_router)
 
 if __name__ == '__main__':
     uvicorn.run('main:app', host='127.0.0.1', port=8000, reload=True)
