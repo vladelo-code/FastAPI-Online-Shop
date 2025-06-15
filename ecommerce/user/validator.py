11b31ee5e1086feb121fdfc28bf5651b0c0fd6b6
vladelo <vlad.lakhtion@gmail.com>
2025-06-15 20:06:53 +0300

    adding docstring in def verify_email_exist

--- ecommerce/user/validator.py
+++ ecommerce/user/validator.py
@@ -1,8 +1,15 @@
-from typing import Optional
 from sqlalchemy.orm import Session
+from typing import Optional
 
-from .models import User
+from ecommerce.user.models import User
 
 
-async def verify_email_exist(email: str, db_session: Session) -> Optional[User]:
-    return db_session.query(User).filter(User.email == email).first()
+async def verify_email_exist(email: str, db: Session) -> Optional[User]:
+    """
+    Проверяет, существует ли пользователь с указанным email.
+
+    :param email: Email пользователя.
+    :param db: Сессия базы данных.
+    :return: Объект User, если найден, иначе None.
+    """
+    return db.query(User).filter(User.email == email).first()
