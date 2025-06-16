f771faca1ed2276ee12c4af0d2c7a7ddcf7d8815
vladelo <vlad.lakhtion@gmail.com>
2025-06-16 09:57:02 +0300

    adding description to app

--- main.py
+++ main.py
@@ -7,7 +7,19 @@ from ecommerce.products.router import router as product_router
 from ecommerce.cart.router import router as cart_router
 from ecommerce.orders.router import router as order_router
 
-app = FastAPI(title='Vladelo Online Shop', version='0.0.1')
+description = '''Полнофункциональный онлайн-магазин, построенный на современном стеке технологий. 
+Проект поддерживает регистрацию пользователей, управление товарами и категориями, корзину, оформление заказов и отправку email-уведомлений. 
+Реализована полноценная аутентификация, асинхронные API, фоновая отправка писем через Celery и Redis, а также покрытие тестами.
+'''
+
+app = FastAPI(title='Vladelo Online Shop',
+              description=description,
+              contact={
+                  "name": "Vladislav Lakhtionov",
+                  "url": "https://t.me/vladelo",
+              },
+              )
+
 app.include_router(auth_router)
 app.include_router(user_router)
 app.include_router(product_router)
